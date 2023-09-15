6042dba59f66f902fd2d8e082dba136bce66f630
Hwang Sangwook <goodbyeyo@naver.com>
2023-09-15 12:44:43 +0900

    create project

---
+++ src/main/java/com/study/batch/ManagementBatchApplication.java
@@ -0,0 +1,13 @@
+package com.study.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/batch/ManagementBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
