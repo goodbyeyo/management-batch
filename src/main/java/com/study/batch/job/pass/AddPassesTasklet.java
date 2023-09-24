74cbc8d1800825ec4d8440328c71418fbff98959
Sangwook Hwang <yoonseol1983@gmail.com>
2023-09-25 01:33:42 +0900

    bulk pass user

--- src/main/java/com/study/batch/job/pass/AddPassesTasklet.java
+++ src/main/java/com/study/batch/job/pass/AddPassesTasklet.java
@@ -1,9 +1,7 @@
 package com.study.batch.job.pass;
 
-import com.study.batch.repository.pass.BulkPassEntity;
-import com.study.batch.repository.pass.BulkPassRepository;
-import com.study.batch.repository.pass.BulkPassStatus;
-import com.study.batch.repository.pass.PassRepository;
+import com.study.batch.repository.pass.*;
+import com.study.batch.repository.user.UserGroupMappingEntity;
 import com.study.batch.repository.user.UserGroupMappingRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
@@ -13,6 +11,7 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -32,14 +31,32 @@ public class AddPassesTasklet implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 
-        // 1. 이용권 시작 1일전 user group 내 각 사용자에게 이용권 추가
+        // 1. 이용권 시작 1일 전 user group 내 각 사용자에게 이용권 추가
         final LocalDateTime startedAt = LocalDateTime.now().minusDays(1);
         final List<BulkPassEntity> bulkPassEntities = bulkPassRepository.findByStatusAndStartedAtGreaterThan(BulkPassStatus.READY, startedAt);
 
+        int count = 0;
         // 2. 대량 이용권 정보로 user group 에 속한 userId 를 조회하고 userId 로 이용권 추가
+        for (BulkPassEntity bulkPassEntity : bulkPassEntities) {
+            final List<String> userIds = userGroupMappingRepository.findByUserGroupId(bulkPassEntity.getUserGroupId())
+                    .stream()
+                    .map(UserGroupMappingEntity::getUserId)
+                    .toList();
+            count += addPasses(bulkPassEntity, userIds);
+            bulkPassEntity.setStatus(BulkPassStatus.COMPLETED);
+        }
 
         // 3. 종료 처리
-
+        log.info("AddPassesTasklet executed. count: {}", count);
         return null;
+    }
+
+    private int addPasses(BulkPassEntity bulkPassEntity, List<String> userIds) {
+        List<PassEntity> passEntities = new ArrayList<>();
+        for (String userId : userIds) {
+            PassEntity passEntity = PassModelMapper.INSTANCE.toPassEntity(bulkPassEntity, userId);
+            passEntities.add(passEntity);
+        }
+        return passRepository.saveAll(passEntities).size();
     }
 }
---
+++ src/main/java/com/study/batch/job/pass/PassModelMapper.java
@@ -0,0 +1,30 @@
+package com.study.batch.job.pass;
+
+
+import com.study.batch.repository.pass.BulkPassEntity;
+import com.study.batch.repository.pass.BulkPassStatus;
+import com.study.batch.repository.pass.PassEntity;
+import com.study.batch.repository.pass.PassStatus;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+// ReportingPolicy.IGNORE: 일치하지 않은 필드를 무시합니다.
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PassModelMapper {
+    PassModelMapper INSTANCE = Mappers.getMapper(PassModelMapper.class);
+
+    // 필드명이 같지 않거나 custom하게 매핑해주기 위해서는 @Mapping을 추가해주면 됩니다.
+    @Mapping(target = "status", qualifiedByName = "defaultStatus")
+    @Mapping(target = "remainingCount", source = "bulkPassEntity.count")
+    PassEntity toPassEntity(BulkPassEntity bulkPassEntity, String userId);
+
+    // BulkPassStatus와 관계 없이 PassStatus값을 설정합니다.
+    @Named("defaultStatus")
+    default PassStatus status(BulkPassStatus status) {
+        return PassStatus.READY;
+    }
+
+}
